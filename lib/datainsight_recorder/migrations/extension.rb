46a302a7dba81d844d2fda09eeec607b7ab39b26
Max Fliri <mfliri@thoughtworks.com>
2013-03-11 11:39:14 +0000

    Add method to drop an index
    @maxfliri
    @robyoung

--- lib/datainsight_recorder/migrations/extension.rb
+++ lib/datainsight_recorder/migrations/extension.rb
@@ -7,6 +7,11 @@ module DataMapper
         statement = "show index from #{quote_name(table)} where key_name = ?"
         select(statement, name).count > 0
       end
+
+      def drop_index(table, name)
+        statement = "alter table #{quote_name(table)} drop index #{quote_name(name)}"
+        execute(statement)
+      end
     end
   end
 end
