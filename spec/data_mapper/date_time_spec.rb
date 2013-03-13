2981eeb05c02a6577a304abc494874327080a0a9
Max Fliri <mfliri@thoughtworks.com>
2013-03-13 10:46:31 +0000

    Test converted to rspec one-liner syntax
    @maxfliri

--- spec/data_mapper/date_time_spec.rb
+++ spec/data_mapper/date_time_spec.rb
@@ -2,25 +2,22 @@ require "spec_helper"
 
 describe DataMapper::Property::DateTime do
 
-  before :each do
+  subject {
     model = double("model", repository_name: "repo")
     name = double("name")
+    DataMapper::Property::DateTime.new(model, name)
+  }
 
-    @date_time_property = DataMapper::Property::DateTime.new(model, name)
-
+  describe "should dump dates as UTC" do
+    it { should dump_property_value(d('2013-03-10 18:30:00+05:00')).as d('2013-03-10 13:30:00+00:00')}
   end
 
-  it "should dump dates as UTC" do
-    dumped_value = @date_time_property.dump(d('2013-03-10 18:30:00+05:00'))
-    dumped_value.should equal_date_time d('2013-03-10 13:30:00+00:00')
-  end
-
-  it "should load summer dates with DST timezone" do
-    @date_time_property.load(d('2013-01-01 14:00:00 +00:00')).should equal_date_time d('2013-01-01 14:00:00 +00:00')
-    @date_time_property.load(d('2013-03-31 00:00:00 +00:00')).should equal_date_time d('2013-03-31 00:00:00 +00:00')
-    @date_time_property.load(d('2013-03-31 01:00:00 +00:00')).should equal_date_time d('2013-03-31 02:00:00 +01:00') # date of switch to BST for 2013 - source: https://www.gov.uk/when-do-the-clocks-change
-    @date_time_property.load(d('2013-06-01 14:00:00 +00:00')).should equal_date_time d('2013-06-01 15:00:00 +01:00')
-    @date_time_property.load(d('2013-10-27 00:00:00 +00:00')).should equal_date_time d('2013-10-27 01:00:00 +01:00')
-    @date_time_property.load(d('2013-10-27 01:00:00 +00:00')).should equal_date_time d('2013-10-27 01:00:00 +00:00') # date of switch back to GMT for 2013 - source: https://www.gov.uk/when-do-the-clocks-change
+  describe "load dates with appropriate DST" do
+    it { should load_property_value(d('2013-01-01 14:00:00 +00:00')).as d('2013-01-01 14:00:00 +00:00') }
+    it { should load_property_value(d('2013-03-31 00:00:00 +00:00')).as d('2013-03-31 00:00:00 +00:00') }
+    it { should load_property_value(d('2013-03-31 01:00:00 +00:00')).as d('2013-03-31 02:00:00 +01:00') } # time of switch to BST for 2013 - source: https://www.gov.uk/when-do-the-clocks-change
+    it { should load_property_value(d('2013-06-01 14:00:00 +00:00')).as d('2013-06-01 15:00:00 +01:00') }
+    it { should load_property_value(d('2013-10-27 00:00:00 +00:00')).as d('2013-10-27 01:00:00 +01:00') }
+    it { should load_property_value(d('2013-10-27 01:00:00 +00:00')).as d('2013-10-27 01:00:00 +00:00') } # tim of switch back to GMT for 2013 - source: https://www.gov.uk/when-do-the-clocks-change
   end
 end
