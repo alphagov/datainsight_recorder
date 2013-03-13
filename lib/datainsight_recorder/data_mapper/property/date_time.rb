7a8bfe983d52e124d0cc73c028a26e8fd8caeb31
Max Fliri <mfliri@thoughtworks.com>
2013-03-13 12:08:42 +0000

    Configurable timezone
    @maxfliri

--- lib/datainsight_recorder/data_mapper/property/date_time.rb
+++ lib/datainsight_recorder/data_mapper/property/date_time.rb
@@ -4,6 +4,7 @@ require "tzinfo"
 module DataMapper
   class Property
     class DateTime
+
       alias_method :base_dump, :dump
       alias_method :base_load, :load
 
@@ -13,13 +14,33 @@ module DataMapper
 
       def load(value)
         date = base_load(value)
-        tz = TZInfo::Timezone.get("Europe/London")
-        date.new_offset(tz.period_for_utc(date).utc_total_offset_rational)
+        date.new_offset(DateTime.timezone.period_for_utc(date).utc_total_offset_rational)
       end
 
       def to_utc(date)
         date.new_offset(0)
       end
+
+      class << self
+        def timezone
+          @timezone ||= TZInfo::Timezone.get("Europe/London")
+        end
+
+        def timezone=(timezone)
+          @timezone = get_timezone(timezone)
+        end
+
+        private
+
+        def get_timezone(timezone)
+          case timezone
+            when TZInfo::Timezone then timezone
+            when ::String then TZInfo::Timezone.get(timezone)
+            else raise ArgumentError.new("'#{timezone}' is not a valid timezone argument")
+          end
+        end
+      end
+
     end
   end
 end
--- spec/data_mapper/date_time_spec.rb
+++ spec/data_mapper/date_time_spec.rb
@@ -2,14 +2,14 @@ require "spec_helper"
 
 describe DataMapper::Property::DateTime do
 
-  subject {
+  subject(:date_time_property) {
     model = double("model", repository_name: "repo")
     name = double("name")
     DataMapper::Property::DateTime.new(model, name)
   }
 
   describe "should dump dates as UTC" do
-    it { should dump_property_value(d('2013-03-10 18:30:00+05:00')).as d('2013-03-10 13:30:00+00:00')}
+    it { should dump_property_value(d('2013-03-10 18:30:00+05:00')).as d('2013-03-10 13:30:00+00:00') }
   end
 
   describe "load dates with appropriate DST" do
@@ -19,5 +19,34 @@ describe DataMapper::Property::DateTime do
     it { should load_property_value(d('2013-06-01 14:00:00 +00:00')).as d('2013-06-01 15:00:00 +01:00') }
     it { should load_property_value(d('2013-10-27 00:00:00 +00:00')).as d('2013-10-27 01:00:00 +01:00') }
     it { should load_property_value(d('2013-10-27 01:00:00 +00:00')).as d('2013-10-27 01:00:00 +00:00') } # tim of switch back to GMT for 2013 - source: https://www.gov.uk/when-do-the-clocks-change
+  end
+
+  describe "timezone configuration" do
+    it "should allow configuration of timezone" do
+      DataMapper::Property::DateTime.timezone = TZInfo::Timezone.get("Europe/Athens")
+      DataMapper::Property::DateTime.timezone.should == TZInfo::Timezone.get("Europe/Athens")
+    end
+
+    it "should allow configuration of timezone as a String" do
+      DataMapper::Property::DateTime.timezone = "Europe/Moscow"
+      DataMapper::Property::DateTime.timezone.should == TZInfo::Timezone.get("Europe/Moscow")
+    end
+
+    it "should raise exception if timezone name is invalid" do
+      lambda { DataMapper::Property::DateTime.timezone = "Europe/Hackney" }.should raise_exception
+    end
+
+    it "should raise exception if timezone argument is neither a timezone or a stirng" do
+      lambda { DataMapper::Property::DateTime.timezone = :PST }.should raise_exception
+    end
+
+    describe "with timezone set to Europe/Rome" do
+      before :all do
+        DataMapper::Property::DateTime.timezone = TZInfo::Timezone.get("Europe/Rome")
+      end
+
+      it { should load_property_value(d('2013-01-01 14:00:00 +00:00')).as d('2013-01-01 15:00:00 +01:00') }
+      it { should load_property_value(d('2013-06-01 14:00:00 +00:00')).as d('2013-06-01 16:00:00 +02:00') }
+    end
   end
 end
